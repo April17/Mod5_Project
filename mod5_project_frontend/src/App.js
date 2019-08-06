cd648ce5b7963b49d809b34869755e74b3343062
April17 <fanmeng0417@gmail.com>
2019-08-06 11:58:05 -0400

    Sprit Assents

--- mod5_project_frontend/src/App.js
+++ mod5_project_frontend/src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
     initialize: false,
     counter: 0,
     game: {
-      width: 160,
-      height: 90,
+      width: 320,
+      height: 180,
       type: Phaser.AUTO,
       scene: {
         init: function() {
@@ -35,6 +35,7 @@ class App extends Component {
           const blocking_object = map.createStaticLayer("blocking_object", tileset, 0, 0);
           const top_layer = map.createStaticLayer("top_layer", tileset, 0, 0);
           const playable_object = map.createStaticLayer("playable_object", tileset, 0, 0);
+          // this.animatedTiles.init(map)
 
           const camera = this.cameras.main;
           const cursors = this.input.keyboard.createCursorKeys();
@@ -47,9 +48,10 @@ class App extends Component {
             speed: 0.15
           });
           camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-          this.scale.setZoom(8.5)
+          this.scale.setZoom(3.5)
         },
-        update: function(time, delta, event) {
+        update: function(time, delta) {
+          // console.log(controls.camera.midPoint);
           controls.update(delta);
         }
       },
@@ -63,12 +65,12 @@ class App extends Component {
   }
 
   initializeGame = () => {
-    const { game } = this.state
+    // const { game } = this.state
     // this.ionPhaser.game = game
     this.setState({ initialize: true })
-    setTimeout(() => {
-      console.log(game.instance.events)
-    }, 3000)
+    // setTimeout(() => {
+    //   console.log(game.instance.events)
+    // }, 3000)
   }
 
   render() {
